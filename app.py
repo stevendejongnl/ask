16cf7dc13cd67b26bb937be3561887eceb8359a5
Steven de Jong <steven@steven-dejong.nl>
2025-07-02 13:41:52 +0000

    joe

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, abort
+from flask import Flask, render_template, request, abort
 from openai import OpenAI
 
 __version__ = '0.1.0'
@@ -20,11 +20,22 @@ def index():
 def ask():
     if request.method == 'POST':
         question = request.form['question']
-        response = client.responses.create(
-            model="gpt-4.1",
-            instructions="Answer super sarcastic.",
-            input=question
+
+        try:
+            response = client.responses.create(
+                model="gpt-4.1",
+                instructions="Answer super sarcastic.",
+                input=question
+            )
+            ai_response = response.output_text[0]['content']['text']
+        except:
+            ai_response = None
+
+        return render_template(
+            'index.html'
+            title='Ask',
+            description='Ask stuff.',
+            ai_response=ai_response,
         )
-        return jsonify(response.output_text)
     
     return abort(405)
