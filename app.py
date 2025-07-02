76ec06696c3f1b198da281f7367a2a920c75f977
Steven de Jong <steven@steven-dejong.nl>
2025-07-02 13:21:21 +0000

    ask stuff

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, abort
+from openai import OpenAI
 
 __version__ = '0.1.0'
 app = Flask(__name__)
 app.secret_key = 'mega-secret-ask'
+
+client = OpenAI()
 
 @app.route('/')
 def index():
@@ -11,3 +14,17 @@ def index():
         title='Ask',
         description='Ask stuff.'
     )
+
+
+@app.route('/ask', methods=['POST'])
+def ask():
+    if request.method == 'POST':
+        question = request.form['question']
+        response = client.responses.create(
+            model="gpt-4.1",
+            instructions="Answer super sarcastic.",
+            input=question
+        )
+        return jsonify(response.output_text)
+    
+    return abort(405)
