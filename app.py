553b474c3162d2a7794570afcbbe75c9ec1045dc
Steven de Jong <steven@steven-dejong.nl>
2025-07-02 13:51:54 +0000

    logger

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from openai import OpenAI
 __version__ = '0.1.0'
 app = Flask(__name__)
 app.secret_key = 'mega-secret-ask'
+app.logger.setLevel(logging.INFO)
 
 client = OpenAI()
 
@@ -21,13 +22,16 @@ def ask():
     if request.method == 'POST':
         question = request.form['question']
 
+        app.logger.info(f"Question: {question}")
+
         try:
             response = client.responses.create(
                 model="gpt-4.1",
                 instructions="Answer super sarcastic.",
                 input=question
             )
-            print(response, response.output_text)
+
+            app.logger.info(f"Ai response: {response.output_text=}")
             ai_response = response.output_text[0]['content']['text']
         except:
             ai_response = None
