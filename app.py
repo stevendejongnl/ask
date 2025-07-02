7f2381cb8f5dbc36056f6b35cad8fb17aa9b5301
Steven de Jong <steven@steven-dejong.nl>
2025-07-02 13:04:17 +0000

    whoeps

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template
-from area.routes import area_blueprint
 
 __version__ = '0.1.0'
 app = Flask(__name__)
 app.secret_key = 'mega-secret-ask'
-
-app.register_blueprint(area_blueprint)
 
 @app.route('/')
 def index():
